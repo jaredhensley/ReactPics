618812a4d517633a0b55f2797073c3d9d78a0979
Jared Hensley <jahensle85@me.com>
2019-06-17 14:22:09 -0500

    handle form submissions with axios and async await

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 01e4529b399b6189956b12e2b02fd105bab67e7f4144ccbade58a6d79b5dc14c'
+            }
+        });
+
+        console.log(response.data.results);
     }
 
     render() {
