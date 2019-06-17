9ed0df081dad9b610f4c6c33932b1a715b3901ac
Jared Hensley <jahensle85@me.com>
2019-06-16 22:45:23 -0500

    handle user input and form submission

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div style={{ margin: '10px' }}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
+}
+
+export default App;
