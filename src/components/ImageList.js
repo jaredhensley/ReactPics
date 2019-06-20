f5db7cdac60df6e776eeca692e7fa5eb7de60004
Jared Hensley <jahensle85@me.com>
2019-06-19 23:12:50 -0500

    css grid for tiling effect + dom refs

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
-const ImageList = (props) => {
-    return props.images.map((image) => {
-        const { description, id, urls } = image;
-        return <img alt={description} key={id} src={urls.regular}></img>;
+const ImageList = props => {
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     })
-    // return <div>Image List</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
